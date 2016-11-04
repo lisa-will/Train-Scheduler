9d4d26f15c02975340621c3a61ed174b8d1c2fce
Lisa Williams <williams.lisarenee@gmail.com>
2016-11-04 01:29:42 -0400

    added content

---
+++ assets/javascript/logic.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyBi7OGXLmvnGfhmkXsRf_d7OOLvxFQu6jo",
+	authDomain: "train-scheduler-7011f.firebaseapp.com",
+	databaseURL: "https://train-scheduler-7011f.firebaseio.com",
+	storageBucket: "train-scheduler-7011f.appspot.com",
+	messagingSenderId: "715906356708"
+};
+
+firebase.initializeApp(config);
